48ba376671853b67c884004c19b011fe63e4156b
Leon <info@leon.vankammen.eu>
2017-07-31 10:32:19 +0200

    v1.2 1st commit

---
+++ css/style.bash
@@ -0,0 +1,7 @@
+black='#333745'
+red='#e63462'
+blue='#0FACF3'
+gray="#fafafa"
+white='#FFFFFF'
+
+fontsize_huge='54px'
---
+++ v1.2/js/Sidebar.js
@@ -0,0 +1,519 @@
+/**********************************
+
+SIDEBAR CODE
+
+**********************************/
+
+function Sidebar(loopy){
+
+	var self = this;
+	PageUI.call(self, document.getElementById("sidebar"));
+
+	// Edit
+	self.edit = function(object){
+		self.showPage(object._CLASS_);
+		self.currentPage.edit(object);
+	};
+
+	// Go back to main when the thing you're editing is killed
+	subscribe("kill",function(object){
+		if(self.currentPage.target==object){
+			self.showPage("Edit");
+		}
+	});
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	// ACTUAL PAGES ////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Node!
+	(function(){
+		var page = new SidebarPage();
+		page.addComponent(new ComponentButton({
+			header: true,
+			label: "back to top",
+			onclick: function(){
+				self.showPage("Edit");
+			}
+		}));
+		page.addComponent("label", new ComponentInput({
+			label: "<br><br>Name:"
+			//label: "Name:"
+		}));
+		page.addComponent("hue", new ComponentSlider({
+			bg: "color",
+			label: "Color:",
+			options: [0,1,2,3,4,5],
+			oninput: function(value){
+				Node.defaultHue = value;
+			}
+		}));
+		page.addComponent("init", new ComponentSlider({
+			bg: "initial",
+			label: "Start Amount:",
+			options: [0, 0.16, 0.33, 0.50, 0.66, 0.83, 1],
+			//options: [0, 1/6, 2/6, 3/6, 4/6, 5/6, 1],
+			oninput: function(value){
+				Node.defaultValue = value;
+			}
+		}));
+
+		var tpl = '{'                                      + "\n"
+		tpl    += '  "type":"node",'                       + "\n"
+		tpl    += '  "onInput": function(signal,process){' + "\n"
+		tpl    += '    signal.delta *= 0.5 // weaken'      + "\n"
+		tpl    += '    process(signal)'                    + "\n"
+		tpl    += '  }'                                    + "\n"
+		tpl    += '}'                                      + "\n"
+
+		var cmp = {                                                                                                                                   
+  		label: "<br><br>Script:",
+			id: "nodeScript", 
+  		defaultValue: "", 
+			placeholder:"click to initialize", 
+  		textarea: true, 
+			onClick: function(){
+					if( this.value == "" ) this.value = tpl
+			}, 
+  		oninput: function(value){
+				Node.defaultValue = value.replace(/(\n|\t)/gi,'').replace(/[ ]+;[ ]+/gi,';')
+  		}
+  	}
+		page.addComponent("jsConfig", new ComponentInput(cmp) )
+
+	  // remember base components
+		page.baseComponents = page.components.slice() 
+
+		page.onedit = function(){
+
+			if( loopy.script && loopy.script.onEvent && !loopy.script.registeredComponents ){
+				page.dom.innerHTML = ""
+				page.components = []
+				page.componentsById = {}
+				page.baseComponents.map( function(c){ page.addComponent(c.propName, c) })
+				loopy.script.onEvent("initNodeGUI", {page:page})
+				loopy.script.registeredComponents = true
+			} 
+			// Set color of Slider
+			var node = page.target;
+			var color = Node.COLORS[node.hue];
+			page.getComponent("init").setBGColor(color);
+
+			// Focus on the name field IF IT'S "" or "?"
+			var name = node.label;
+			if(name=="" || name=="?") page.getComponent("label").select();
+
+		};
+		page.addComponent(new ComponentButton({
+			label: "delete node",
+			//label: "delete circle",
+			onclick: function(node){
+				node.kill();
+				self.showPage("Edit");
+			}
+		}));
+		self.addPage("Node", page);
+	})();
+
+	// Edge!
+	(function(){
+		var page = new SidebarPage();
+		page.addComponent(new ComponentButton({
+			header: true,
+			label: "back to top",
+			onclick: function(){
+				self.showPage("Edit");
+			}
+		}));
+		page.addComponent("strength", new ComponentSlider({
+			bg: "strength",
+			label: "<br><br>Relationship:",
+			//label: "Relationship:",
+			options: [1, -1],
+			oninput: function(value){
+				Edge.defaultStrength = value;
+			}
+		}));
+		page.addComponent(new ComponentHTML({
+			html: "(to make a stronger relationship, draw multiple arrows!)<br><br>"+
+			"(to make a delayed relationship, draw longer arrows)"
+		}));
+		page.addComponent(new ComponentButton({
+			//label: "delete edge",
+			label: "delete arrow",
+			//label: "delete relationship",
+			onclick: function(edge){
+				edge.kill();
+				self.showPage("Edit");
+			}
+		}));
+		self.addPage("Edge", page);
+	})();
+
+	// Label!
+	(function(){
+		var page = new SidebarPage();
+		page.addComponent(new ComponentButton({
+			header: true,
+			label: "back to top",
+			onclick: function(){
+				self.showPage("Edit");
+			}
+		}));
+		page.addComponent("text", new ComponentInput({
+			label: "<br><br>Label:",
+			//label: "Label:",
+			textarea: true
+		}));
+		page.onshow = function(){
+			// Focus on the text field
+			page.getComponent("text").select();
+		};
+		page.onhide = function(){
+			
+			// If you'd just edited it...
+			var label = page.target;
+			if(!page.target) return;
+
+			// If text is "" or all spaces, DELETE.
+			var text = label.text;
+			if(/^\s*$/.test(text)){
+				// that was all whitespace, KILL.
+				page.target = null;
+				label.kill();
+			}
+
+		};
+		page.addComponent(new ComponentButton({
+			label: "delete label",
+			onclick: function(label){
+				label.kill();
+				self.showPage("Edit");
+			}
+		}));
+		self.addPage("Label", page);
+	})();
+
+	// Edit
+	(function(){
+		var page = new SidebarPage();
+		page.addComponent(new ComponentHTML({
+			html: ""+
+			
+			"<b style='font-size:1.4em'>LOOPY</b> (v1.1)<br>a tool for thinking in systems<br><br>"+
+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"examples\"])'>see examples</span> "+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"howto\"])'>how to</span> "+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"credits\"])'>credits</span><br><br>"+
+
+			"<hr/><br>"+
+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"save_link\"])'>save as link</span> <br><br>"+
+			//"<span class='mini_button' onclick='publish(\"modal\",[\"save_image\"])'>save as image</span> <br><br>"+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"embed\"])'>embed in your website</span> <br><br>"+
+
+			"<hr/><br>"
+
+		}));
+		var tpl = '{'                             + "\n"
+		tpl    += '  debug: true,'                + "\n"
+		tpl    += '  embed: {'                    + "\n"
+		tpl    += '    autoPlay: true,'           + "\n"
+		tpl    += '    autoSignal: ["something"],'+ "\n"
+		tpl    += '    hideButtons: true'         + "\n"
+		tpl    += '  }'                           + "\n"
+		tpl    += '}'                             + "\n"
+		var cmp = {                                                                                                                                   
+  		label: "<br><br>Script:",
+			id: "globalScript", 
+  		defaultValue: "", 
+			placeholder:"click to initialize", 
+  		textarea: true, 
+			onClick: function(){
+					if( this.value == "" ) this.value = tpl
+			}, 
+  		oninput: function(value){
+				Node.defaultValue = value.replace(/(\n|\t)/gi,'').replace(/[ ]+;[ ]+/gi,';')
+					try{
+  					var script = new Function("return "+Node.defaultValue)
+  					loopy.script = {
+  							version:"1.1", 
+  							onEvent: function(event, data){
+  									if( event == "init" ){
+ 											if( script.embed ) 
+											if( script.onEvent ) script.onEvent(event, data)
+  									}
+  							}
+  					}
+					}catch(e){
+		  				console.error( "global scripterror: "+e )
+		  				console.dir(e)
+					}
+  		}
+  	}
+		page.addComponent("globalConfig", new ComponentInput(cmp) )
+
+		self.addPage("Edit", page);
+	})();
+
+	// Ctrl-S to SAVE
+	subscribe("key/save",function(){
+		if(Key.control){ // Ctrl-S or ⌘-S
+			publish("modal",["save_link"]);
+		}
+	});
+
+}
+
+function SidebarPage(){
+
+	// TODO: be able to focus on next component with an "Enter".
+
+	var self = this;
+	self.target = null;
+
+	// DOM
+	self.dom = document.createElement("div");
+	self.show = function(){ self.dom.style.display="block"; self.onshow(); };
+	self.hide = function(){ self.dom.style.display="none"; self.onhide(); };
+
+	// Components
+	self.components = [];
+	self.componentsByID = {};
+	self.addComponent = function(propName, component){
+
+		// One or two args
+		if(!component){
+			component = propName;
+			propName = "";
+		}
+
+		component.page = self; // tie to self
+		component.propName = propName; // tie to propName
+		self.dom.appendChild(component.dom); // add to DOM
+
+		// remember component
+		self.components.push(component);
+		self.componentsByID[propName] = component;
+
+		// return!
+		return component;
+
+	};
+	self.getComponent = function(propName){
+		return self.componentsByID[propName];
+	};
+
+	// Edit
+	self.edit = function(object){
+
+		// New target to edit!
+		self.target = object;
+
+		// Show each property with its component
+		for(var i=0;i<self.components.length;i++){
+			self.components[i].show();
+		}
+
+		// Callback!
+		self.onedit();
+
+	};
+
+	// TO IMPLEMENT: callbacks
+	self.onedit = function(){};
+	self.onshow = function(){};
+	self.onhide = function(){};
+
+	// Start hiding!
+	self.hide();
+
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// COMPONENTS ///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+function Component(){
+	var self = this;
+	self.dom = null;
+	self.page = null;
+	self.propName = null;
+	self.show = function(){
+		// TO IMPLEMENT
+	};
+	self.getValue = function(){
+		return self.page.target[self.propName];
+	};
+	self.setValue = function(value){
+		
+		// Model's been changed!
+		publish("model/changed");
+
+		// Edit the value!
+		self.page.target[self.propName] = value;
+		self.page.onedit(); // callback!
+		
+	};
+}
+
+function ComponentInput(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// DOM: label + text input
+	self.dom = document.createElement("div");
+	var label = _createLabel(config.label);
+	var className = config.textarea ? "component_textarea" : "component_input";
+	var input = _createInput(className, config);
+	input.oninput = function(event){
+		self.setValue(input.value);
+	};
+	self.dom.appendChild(label);
+	self.dom.appendChild(input);
+
+	// Show
+	self.show = function(){
+		input.value = self.getValue() ? self.getValue() : String( config.defaultValue != undefined ? config.defaultValue : "" );
+	};
+
+	// Select
+	self.select = function(){
+		setTimeout(function(){ input.select(); },10);
+	};
+
+}
+
+function ComponentSlider(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// TODO: control with + / -, alt keys??
+
+	// DOM: label + slider
+	self.dom = document.createElement("div");
+	var label = _createLabel(config.label);
+	self.dom.appendChild(label);
+	var sliderDOM = document.createElement("div");
+	sliderDOM.setAttribute("class","component_slider");
+	self.dom.appendChild(sliderDOM);
+
+	// Slider DOM: graphic + pointer
+	var slider = new Image();
+	slider.draggable = false;
+	slider.src = "css/sliders/"+config.bg+".png";
+	slider.setAttribute("class","component_slider_graphic");
+	var pointer = new Image();
+	pointer.draggable = false;
+	pointer.src = "css/sliders/slider_pointer.png";
+	pointer.setAttribute("class","component_slider_pointer");
+	sliderDOM.appendChild(slider);
+	sliderDOM.appendChild(pointer);
+	var movePointer = function(){
+		var value = self.getValue();
+		var optionIndex = config.options.indexOf(value);
+		var x = (optionIndex+0.5) * (250/config.options.length);
+		pointer.style.left = (x-7.5)+"px";
+	};
+
+	// On click... (or on drag)
+	var isDragging = false;
+	var onmousedown = function(event){
+		isDragging = true;
+		sliderInput(event);
+	};
+	var onmouseup = function(){
+		isDragging = false;
+	};
+	var onmousemove = function(event){
+		if(isDragging) sliderInput(event);
+	};
+	var sliderInput = function(event){
+
+		// What's the option?
+		var index = event.x/250;
+		var optionIndex = Math.floor(index*config.options.length);
+		var option = config.options[optionIndex];
+		if(option===undefined) return;
+		self.setValue(option);
+
+		// Callback! (if any)
+		if(config.oninput){
+			config.oninput(option);
+		}
+
+		// Move pointer there.
+		movePointer();
+
+	};
+	_addMouseEvents(slider, onmousedown, onmousemove, onmouseup);
+
+	// Show
+	self.show = function(){
+		movePointer();
+	};
+
+	// BG Color!
+	self.setBGColor = function(color){
+		slider.style.background = color;
+	};
+
+}
+
+function ComponentButton(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// DOM: just a button
+	self.dom = document.createElement("div");
+	var button = _createButton(config.label, function(){
+		config.onclick(self.page.target);
+	});
+	self.dom.appendChild(button);
+
+	// Unless it's a HEADER button!
+	if(config.header){
+		button.setAttribute("header","yes");
+	}
+
+}
+
+function ComponentHTML(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// just a div
+	self.dom = document.createElement("div");
+	self.dom.innerHTML = config.html;
+
+}
+
+function ComponentOutput(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// DOM: just a readonly input that selects all when clicked
+	self.dom = _createInput("component_output");
+	self.dom.setAttribute("readonly", "true");
+	self.dom.onclick = function(){
+		self.dom.select();
+	};
+
+	// Output the string!
+	self.output = function(string){
+		self.dom.value = string;
+	};
+
+}
